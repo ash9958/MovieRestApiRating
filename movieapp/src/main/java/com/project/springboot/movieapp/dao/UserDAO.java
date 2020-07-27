b511bef0529584d3b7ea4dc08204299a79a5da13
Faruk <Faruk@DESKTOP-3RBP340>
2020-07-27 12:30:40 +0530

    pom updated with Security Dependency

--- movieapp/src/main/java/com/project/springboot/movieapp/dao/UserDAO.java
+++ movieapp/src/main/java/com/project/springboot/movieapp/dao/UserDAO.java
@@ -1,9 +1,11 @@
 package com.project.springboot.movieapp.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.project.springboot.movieapp.entity.User;
 
 public interface UserDAO extends JpaRepository<User, Integer> {
-
+	public Optional<User> findByName(String name);
 }
