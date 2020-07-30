c1acb6a15fc49f7e333aa25bbccd2419c6ba7212
Faruk <Faruk@DESKTOP-3RBP340>
2020-07-30 12:10:17 +0530

    user updated

--- movieapp/src/main/java/com/project/springboot/movieapp/controller/UserController.java
+++ movieapp/src/main/java/com/project/springboot/movieapp/controller/UserController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.springboot.movieapp.dao.UserDAO;
-import com.project.springboot.movieapp.vo.dto.UserDto;
-import com.project.springboot.movieapp.vo.dto.mapper.UserDtoMapper;
 import com.project.springboot.movieapp.vo.entity.User;
 
 @RestController
@@ -19,14 +17,12 @@ public class UserController {
 	@Autowired
 	UserDAO userDao;
 
-	@GetMapping(value = {"/user","/"})
+	@GetMapping(value = { "/user", "/" })
 	public String getUser(Principal principal) {
 		Optional<User> user = userDao.findByName(principal.getName());
 		user.orElseThrow(
 				() -> new UsernameNotFoundException("No User available with the name -" + principal.getName()));
 
-		UserDto userDto = new UserDtoMapper().getDtoFromUser(user.get());
-		return "<h2>Hii "+userDto.getName()+"( Id-"+userDto.getId()+")</h2><br><br>"+
-				"<a href='http://localhost:8080/logout'>Logout</a>";
+		return "Successfully Logged In";
 	}
 }
